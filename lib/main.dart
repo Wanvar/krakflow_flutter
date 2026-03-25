89c4dc321a46ca50ab58a05eb2f95a6095ee453e
Adrian Pietrzyk <adiii@poczta.fm>
2026-03-25 15:37:55 +0100

    lab_04 krakflow task list - praca w domu

--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,98 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   List<Task> tasks = [
-    Task(title: "test z programowania", deadline: "jutro"),
-    Task(title: "podlać petunie", deadline: "dzisiaj"),
-    Task(title: "zrobić pranie", deadline: "w tym tygodniu"),
-    Task(title: "wymienić olej w samochodzie", deadline: "w tym miesiącu")
+    Task(
+      title: "nauka do kolokwium",
+      deadline: "jutro",
+      done: true,
+      priority: Priority.High,
+    ),
+    Task(
+      title: "podlać petunie",
+      deadline: "dzisiaj",
+      done: true,
+      priority: Priority.Low,
+    ),
+    Task(
+      title: "zrobić pranie",
+      deadline: "w tym tygodniu",
+      done: false,
+      priority: Priority.Medium,
+    ),
+    Task(
+      title: "wymienić olej w samochodzie",
+      deadline: "w tym miesiącu",
+      done: false,
+      priority: Priority.High,
+    ),
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    IconData icon = IconData(0xe146, fontFamily: 'MaterialIcons');
-
     return MaterialApp(
       home: Scaffold(
-
         appBar: AppBar(title: Center(child: Text("Krakflow"))),
 
-        body:
+        body: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Masz dziś ${tasks.length} zadania \n"
+                "Wykonano do tej pory ${tasks.where((e) => e.done).length} zadania",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.indigoAccent,
+                ),
+              ),
 
+              SizedBox(height: 32),
+              Center(
+                child: Text(
+                  "Dzisiejsze zadania",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+                    IconData icon = task.done
+                        ? Icons.check_circle
+                        : Icons.radio_button_unchecked;
 
-        ListView.builder(
-            itemCount: tasks.length,
-            itemBuilder: (context, index){
-              final task = tasks[index];
-              return TaskCard(title: task.title, subtitle: task.deadline, icon: icon);
-            }
-
+                    var priorityMsg = switch (task.priority) {
+                      Priority.Low => 'niski',
+                      Priority.Medium => 'średni',
+                      Priority.High => 'wysoki',
+                      _ => 'nieznany',
+                    };
+                    return Column(
+                      children: [
+                        TaskCard(
+                          title: task.title,
+                          subtitle:
+                              "termin: ${task.deadline} | priorytet: $priorityMsg",
+                          icon: icon,
+                        ),
+                        SizedBox(height: 12),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-
+        ),
       ),
     );
   }
@@ -43,19 +105,29 @@ class MyApp extends StatelessWidget {
 class Task {
   final String title;
   final String deadline;
-  Task({required this.title, required this.deadline});
+  final bool done;
+  final Priority priority;
+
+  Task({
+    required this.title,
+    required this.deadline,
+    required this.done,
+    required this.priority,
+  });
 }
 
 class TaskCard extends StatelessWidget {
   final String title;
   final String subtitle;
   final IconData icon;
+
   const TaskCard({
     super.key,
     required this.title,
     required this.subtitle,
     required this.icon,
   });
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -68,3 +140,4 @@ class TaskCard extends StatelessWidget {
   }
 }
 
+enum Priority { Low, Medium, High }
